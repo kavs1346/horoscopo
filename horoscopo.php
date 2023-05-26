0d531928b81cb4d45b6f8160899238c8c87667b8
kavs1346 <130669072+kavs1346@users.noreply.github.com>
2023-05-26 09:42:59 -0300

    Create horoscopo.php

---
+++ horoscopo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Horoscopo</title>
+</head>
+<body>
+    <form action="pagina2.php" method="get">
+        <br><label for="lblData">Escolha uma data: </label>
+        <input type="date" name="data">
+        <input type="submit">
+    </form>
+</body>
+</html>
