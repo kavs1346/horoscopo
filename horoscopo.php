6c18b8a98d2814540968b5081bac34e0abc9632c
kavs1346 <130669072+kavs1346@users.noreply.github.com>
2023-05-26 09:43:43 -0300

    commit

--- horoscopo.php
+++ horoscopo.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Horoscopo</title>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
 </head>
 <body>
-    <form action="pagina2.php" method="get">
+    <form action="horoscopogilberto2.php" method="get">
         <br><label for="lblData">Escolha uma data: </label>
         <input type="date" name="data">
         <input type="submit">
